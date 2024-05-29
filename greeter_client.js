509004b8b267f768b1b38c1b2136722378746a63
@ELTOROIT - Andres Perez <js4Iot@gmail.com>
2024-05-29 17:51:35 -0400

    Salesforce demo using this code: https://github.com/pozil/pub-sub-api-node-client

--- greeter_client.js
+++ greeter_client.js
@@ -1,41 +1,28 @@
-const grpc = require("@grpc/grpc-js");
-const protoLoader = require("@grpc/proto-loader");
-const packageDefinition = protoLoader.loadSync("@ELTOROIT/protos/ping-pong.proto", {
-	keepCase: true,
-	longs: String,
-	enums: String,
-	defaults: true,
-	oneofs: true,
-});
-const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
-const nsProto = protoDescriptor.pingPong;
+import PubSubApiClient from "salesforce-pubsub-api-client";
 
-function main() {
-	let times = 0;
-	let messages = [];
-	const target = "localhost:50051";
-	const client = new nsProto.Game(target, grpc.credentials.createInsecure());
+async function run() {
+	try {
+		const client = new PubSubApiClient();
+		await client.connect();
 
-	const loop = () => {
-		messages.push({ dttm: new Date().toJSON(), msg: "> PING" });
-		console.log(`${new Date().toJSON()} | >>> PING | sayHello`);
-		client.ping({ data: JSON.stringify(messages) }, function (err, response) {
-			console.log(`${new Date().toJSON()} | <<< PONG | sayHello`);
-			messages = JSON.parse(response.data);
+		// Subscribe to account change events
+		const eventEmitter = await client.subscribe("/data/AccountChangeEvent");
 
-			times++;
-			if (times < 5) {
-				setTimeout(() => {
-					loop();
-				}, 1e3);
-			} else {
-				console.log(messages);
-			}
+		// Handle incoming events
+		eventEmitter.on("data", (event) => {
+			console.log(
+				`Handling ${event.payload.ChangeEventHeader.entityName} change event ` +
+					`with ID ${event.replayId} ` +
+					`on channel ${eventEmitter.getTopicName()} ` +
+					`(${eventEmitter.getReceivedEventCount()}/${eventEmitter.getRequestedEventCount()} ` +
+					`events received so far)`
+			);
+			console.log(event);
+			console.log(JSON.stringify(event, null, 2));
 		});
-	};
-
-	messages.push({ dttm: new Date().toJSON(), msg: "ELTOROit | Initialized" });
-	loop();
+	} catch (error) {
+		console.error(error);
+	}
 }
 
-main();
+run();
