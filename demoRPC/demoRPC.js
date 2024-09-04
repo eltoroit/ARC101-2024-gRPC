27d7c67eb264da65fbe69c79bb982f465d38917c
@ELTOROIT - Andres Perez <andresperezf@yahoo.com>
2024-09-04 14:19:24 -0400

    Version 2.0

--- demoRPC/demoRPC.js
+++ demoRPC/demoRPC.js
@@ -2,12 +2,16 @@ class DemoRPC {
 	async simpleCode() {
 		let i = 2;
 		let j = 3;
-		let k = i + j;
+		let k = await this.add(i, j);
 		return k;
+	}
+
+	async add(i, j) {
+		return i + j;
 	}
 }
 
 let demo = new DemoRPC();
 demo.simpleCode().then((k) => {
-	console.log(`No procedures called: ${k}`);
+	console.log(`Local procedure called: ${k}`);
 });
